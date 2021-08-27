307397432c4ed5f1d88801b3daf8e4cd8eb2ee54
BiggieFudge <73502334+BiggieFudge@users.noreply.github.com>
2021-08-27 03:08:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,18 +14,19 @@ RED = (255, 0, 0)
 # Open a new window
 size = (900, 900)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Maman Sucks Dicks")
+pygame.display.set_caption("FUN SNAKE")
 
-choice1 = pygame.image.load(r'C:\Users\eytan\PycharmProjects\PySnake-master\sprites\choice.png')
-choice2 = pygame.image.load(r'C:\Users\eytan\PycharmProjects\PySnake-master\sprites\choice.png')
-b1 = pygame.image.load(r'C:\Users\eytan\PycharmProjects\PySnake-master\sprites\button1.png')
-b2 = pygame.image.load(r'C:\Users\eytan\PycharmProjects\PySnake-master\sprites\button2.png')
-b3 = pygame.image.load(r'C:\Users\eytan\PycharmProjects\PySnake-master\sprites\button3.png')
-fixedwalls = pygame.image.load(r'C:\Users\eytan\PycharmProjects\PySnake-master\sprites\button_Fixed_Walls.png')
-transwalls = pygame.image.load(r'C:\Users\eytan\PycharmProjects\PySnake-master\sprites\button_go-through-walls.png')
-play = pygame.image.load(r'C:\Users\eytan\PycharmProjects\PySnake-master\sprites\button_play.png')
-choiceplay = pygame.image.load(r'C:\Users\eytan\PycharmProjects\PySnake-master\sprites\choiceplay.png')
-# The loop will carry on until the user exit the game (e.g. clicks the close button).
+
+choice1 = pygame.image.load(r'D:\Visual Studio repos\PySnake-master\sprites\choice.png')
+choice2 = pygame.image.load(r'D:\Visual Studio repos\PySnake-master\sprites\choice.png')
+b1 = pygame.image.load(r'D:\Visual Studio repos\PySnake-master\sprites\button1.png')
+b2 = pygame.image.load(r'D:\Visual Studio repos\PySnake-master\sprites\button2.png')
+b3 = pygame.image.load(r'D:\Visual Studio repos\PySnake-master\sprites\button3.png')
+fixedwalls = pygame.image.load(r'D:\Visual Studio repos\PySnake-master\sprites\button_Fixed_Walls.png')
+transwalls = pygame.image.load(r'D:\Visual Studio repos\PySnake-master\sprites\button_go-through-walls.png')
+play = pygame.image.load(r'D:\Visual Studio repos\PySnake-master\sprites\button_play.png')
+choiceplay = pygame.image.load(r'D:\Visual Studio repos\PySnake-master\sprites\choiceplay.png')
+
 carryOn = True
 
 # The clock will be used to control how fast the screen updates
@@ -83,7 +84,7 @@ def menu():
                     choicewalls += 1
                 if event.key == pygame.K_LEFT and choice == 2 and choicewalls > 1:
                     choicewalls -= 1
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE or event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN:
                     carrymenu = False
 
         screen.fill(WHITE)
@@ -102,28 +103,29 @@ def menu():
         if choice == 1:
             if animateup == 244:
                 up = 1
-            elif animateup == 253:
+            elif animateup == 250:
                 up = 0
-            if animateup < 256 and up == 1:
+            if animateup < 250 and up == 1:
                 animateup += 0.5
             else:
                 animateup -= 0.5
         elif choice == 2:
             if animatedown == 494:
                 up = 1
-            elif animatedown == 503:
+            elif animatedown == 500:
                 up = 0
-            if animatedown < 503 and up == 1:
+            if animatedown < 500 and up == 1:
                 animatedown += 0.5
             else:
                 animatedown -= 0.5
+
 
         clock.tick(60)
 
 
 init()
 font = pygame.font.Font('freesansbold.ttf', 32)  # create a text surface object,
-text = font.render(str(snake_size), True, (0,0,0))   # on which text is drawn on it.
+# on which text is drawn on it.
 menu()
 
 
@@ -135,9 +137,19 @@ def getbigger():     #  Snake Eating Apple
     genrate_apple()
 
 
+start = 0
+
+
+def starttime():
+    global start
+    if start == 0:
+        start = time.time()
+
 def move():
     global snake_size
-    if snake_size == 1:
+
+    if snake_size == 1 and Direction != 0:
+        starttime()
         if Direction == 2:
             if x[0] - 1 == apple[0] and y[0] == apple[1]:
                 getbigger()
@@ -158,9 +170,10 @@ def move():
                 getbigger()
             else:
                 y[0] = y[0] + 1
-    else:
+    elif Direction != 0:
+        starttime()
         if Direction == 2:
-            if x[0] - 1 == apple[0] and y[0] == apple[1]:
+            if x[0] - 1 == apple[0] and y[0] == apple[1]:  # eats an apple
                 getbigger()
             else:
                 x.insert(0, x.pop(-1))
@@ -195,33 +208,49 @@ def move():
     time.sleep(0.25 / choicediff)
 
 
-def out_of_bounds():
+def out_of_bounds():  # EndGame
     global snake_size
-    if x[0] >= rows:
-        exit(1)
-    elif x[0] < 0:
-        exit(1)
-    if y[0] >= cols:
-        exit(1)
-    elif y[0] < 0:
-        exit(1)
+    if choicewalls == 1:
+        if x[0] >= rows:
+            exit(1)
+        elif x[0] < 0:
+            exit(1)
+        if y[0] >= cols:
+            exit(1)
+        elif y[0] < 0:
+            exit(1)
+    else:
+        if x[0] == rows:
+            x[0] = 0
+        elif x[0] == -1:
+            x[0] = rows-1
+        if y[0] == cols:
+            y[0] = 0
+        elif y[0] == -1:
+            y[0] = cols - 1
     for i in range(1, snake_size - 1):
         if x[i] == x[0] and y[i] == y[0]:
             exit(1)
 
 
 
-start = 0
-# -------- Main Program Loop -----------
 
+#  def pausegame()
+
+game_state= True #   {True - for running, False - for pause
+pause_time = 0
 while carryOn:
+
     # --- Main event loop
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             carryOn = False  # Flag that we are done so we exit this loop
         elif event.type == pygame.KEYDOWN:
-            if start == 0:
-                start = time.time()
+            if event.key == pygame.K_ESCAPE and start != 0:
+                if game_state:
+                    game_state = False
+                else:
+                    game_state = True
             if event.key == pygame.K_UP and Direction != 3:
                 Direction = 1
                 break
@@ -235,35 +264,39 @@ while carryOn:
                 Direction = 4
                 break
 
-        # --- Game logic should go here
-        #TODO Add Graphics
+
+        # TODO Add Graphics
         #TODO Add powerup
-        #TODO transparent walls
-        #TODO PreGame settings(speed , walls, power-ups)
+        #TODO Add pause game
+        #TODO 31 long
 
 
 
         # --- Drawing code should go here
-        # First, clear the screen to white.
+    screen.fill(WHITE)   # First, clear the screen to white.
+    if game_state:
 
-    screen.fill(WHITE)
-    # The you can draw different shapes and lines or add text to your background stage.
+        # The you can draw different shapes and lines or add text to your background stage.
 
-    text = font.render(str(snake_size), True, (0, 0, 0))
-    screen.blit(text, (0, 0))
-    if start != 0:
-        timer = font.render(str("%.2f" % (time.time()-start)), True, (0,0,0))
+        text = font.render(str(snake_size), True, (0, 0, 0))
+        screen.blit(text, (0, 0))
+        stopwatch = time.time() - start
+        if start==0:
+            stopwatch= 0
+        timer = font.render(str("%d" %(stopwatch - pause_time)), True, (0,0,0))
+        screen.blit(timer, (800, 0))
+        for index in range(snake_size):
+            pygame.draw.rect(screen, BLACK, [x[index]*30, y[index]*30, 30, 30])  #  print snake
+        pygame.draw.rect(screen, RED, [apple[0]*30, apple[1]*30, 30, 30])   #  print apple
+        move()
+        pygame.display.flip()
+        out_of_bounds()
+        # --- Go ahead and update the screen with what we've drawn.
     else:
-        timer = font.render(str("Hello"), True, (0,0,0))
-    screen.blit(timer, (800, 0))
-    for index in range(snake_size):
-        pygame.draw.rect(screen, BLACK, [x[index]*30, y[index]*30, 30, 30])  #  print snake
-    pygame.draw.rect(screen, RED, [apple[0]*30, apple[1]*30, 30, 30])   #  print apple
-    move()
-    pygame.display.flip()
-    out_of_bounds()
-    # --- Go ahead and update the screen with what we've drawn.
-
+        text = font.render("Paused", True , (0,0,0))
+        screen.blit(text,(380 ,420))
+        pygame.display.flip()
+        pause_time =time.time() -(start +stopwatch)
 
     clock.tick(60)  # --- Limit to 60 frames per second
 
